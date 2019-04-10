28bbd52485236d6c542334ded1375d5f6649a992
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2019-04-10 09:09:42 +0200

    added animation to the FAB

---
+++ NavigationDrawerDemo/app/src/main/java/com/example/navigationdrawerdemo/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.navigationdrawerdemo;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private FloatingActionButton fab;
+    private TranslateAnimation translateAnimation;
+    Map<String,Integer> dataset;
+    private String[] names={"bird","cow","elephant","dog"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initDataset();
+        recyclerView =findViewById(R.id.recyclerViewID);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter=new MyAdapter(dataset,names);
+        recyclerView.setAdapter(adapter);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        drawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        fab=findViewById(R.id.fab);
+        translateAnimation=new TranslateAnimation(0,-480,0,0);
+        translateAnimation.setDuration(500);
+        translateAnimation.setRepeatCount(1);
+        translateAnimation.setRepeatMode(Animation.REVERSE);
+        //TranslateAnimation (int fromXType,
+        //                float fromXValue,
+        //                int toXType,
+        //                float toXValue,
+        //                int fromYType,
+        //                float fromYValue,
+        //                int toYType,
+        //                float toYValue)
+
+        fab.setOnClickListener((event)->{
+            fab.startAnimation(translateAnimation);
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            drawerLayout.openDrawer(Gravity.START);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void initDataset(){
+        dataset= new HashMap<>();
+        dataset.put("0",R.drawable.bird);
+        dataset.put("1",R.drawable.cow);
+        dataset.put("2",R.drawable.elephant);
+        dataset.put("3",R.drawable.dog);
+    }
+}
---
+++ NavigationDrawerDemo/app/src/main/java/com/example/navigationdrawerdemo/MyAdapter.java
@@ -0,0 +1,54 @@
+package com.example.navigationdrawerdemo;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.util.Map;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>  {
+    private Map<String,Integer> Dataset;
+    private String[] names;
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView textView;
+        CardView cardView;
+        FrameLayout frameLayout;
+        public MyViewHolder(View v) {
+            super(v);
+            textView=v.findViewById(R.id.info_text);
+            frameLayout=v.findViewById(R.id.frame_layout);
+
+        }
+    }
+    public MyAdapter(Map<String,Integer> Dataset, String[] names) {
+        this.Dataset = Dataset;
+        this.names=names;
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View v=LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.mycardview,viewGroup,false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int position) {
+        myViewHolder.textView.setText(names[position]);
+        myViewHolder.frameLayout.setBackgroundResource(Dataset.get(String.valueOf(position)));
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return Dataset.size();
+    }
+}
